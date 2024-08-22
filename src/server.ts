804d55d4cb92a03d71b9e1a635800af05998d254
oussama <oussamabechichi@gmail.com>
2024-08-22 16:22:46 +0200

    added dynamic creation of the upload folder

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,14 @@
+import fs from "fs";
+import path from "path";
 import app from "./app";
+
+// Chemin du répertoire uploads
+const uploadDir = path.resolve(__dirname, "../uploads");
+
+// Vérifier si le répertoire 'uploads' existe, sinon le créer
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const PORT = process.env.PORT || 3000;
 
